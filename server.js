f7d01aca0ab2339bf7f87e82df4b2031b6f30001
dangerthaproducer-pixel <dangerthaproducer@gmail.com>
2026-06-29 13:52:44 -0700

    Add CORS headers for browser uploads

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 const app = express();
 app.use(express.json({ limit: "10mb" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 
 // Persistent audio storage dir
 const AUDIO_DIR = path.join(os.tmpdir(), "yt-audio-store");
